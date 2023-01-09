bdc3f6e3df748dc448c321820abacc4e051d3196
Daria <72020978+kkkovalllchuk@users.noreply.github.com>
2023-01-09 14:17:55 +0200

    Add files via upload

---
+++ populate.sql
@@ -0,0 +1,46 @@
+INSERT INTO Main (m_id  ,title,author)
+VALUES('0', 'Climate action needs new frontline leadership', 'Ozawa Bineshi Albert');
+INSERT INTO Main (m_id  ,title,author)
+VALUES('1', 'The dark history of the overthrow of Hawaii', 'Sydney Iaukea');
+INSERT INTO Main (m_id  ,title,author)
+VALUES('2', 'How play can spark new ideas for your business', 'Martin Reeves');
+INSERT INTO Main (m_id  ,title,author)
+VALUES('3', 'Why is China appointing judges to combat..', 'James K. Thornton');
+INSERT INTO Main (m_id  ,title,author)
+VALUES('4', 'Cements carbon problem — and 2 ways to fix it', 'Mahendra Singhi');
+INSERT INTO Main (m_id  ,title,author)
+VALUES('5','Earths mysterious red glow, explained','Zoe Pierrat');
+INSERT INTO Main (m_id  ,title,author)
+VALUES('6','What is MSG, and is it actually bad for you?','Sarah E. Tracy');
+
+	   
+INSERT INTO Date_ted (date_id, date,m_id)
+VALUES('0', 'December 2021', '0');
+INSERT INTO Date_ted (date_id, date,m_id)
+VALUES('1', 'February 2022', '1');
+INSERT INTO Date_ted (date_id, date,m_id)
+VALUES('2', 'September 2021', '2');
+INSERT INTO Date_ted (date_id, date,m_id)
+VALUES('3', 'October 2021', '3');
+INSERT INTO Date_ted (date_id, date,m_id)
+VALUES('4', 'October 2021', '4');
+insert into date_ted (date_id, date,m_id)
+VALUES('5','September 2021','5');
+insert into date_ted (date_id, date,m_id)
+VALUES('6','September 2021','6');
+
+	   
+INSERT INTO views_likes(v_l_id, views , likes , date_id)
+VALUES('0', 404000, 12000, '0');
+INSERT INTO views_likes(v_l_id, views , likes , date_id)
+VALUES('1', 214000, 6400, '1');
+INSERT INTO views_likes(v_l_id, views , likes , date_id)
+VALUES('2', 412000, 12000, '2');
+INSERT INTO views_likes(v_l_id, views , likes , date_id)
+VALUES('3', 427000,12000, '3');
+INSERT INTO views_likes(v_l_id, views , likes , date_id)
+VALUES('4',2400,72, '4');
+insert into views_likes (v_l_id, views , likes , date_id)
+VALUES('5','583000','17000','5');
+insert into views_likes (v_l_id, views , likes , date_id)
+VALUES('6','2100000','64000','6');
